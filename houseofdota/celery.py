7225d7e528bab25f363933e7639d2fbf271e8218
Lucas Hanke <lhanke@thoughtworks.com>
2016-07-30 03:16:13 -0300

    removed test task from celery config file

--- houseofdota/celery.py
+++ houseofdota/celery.py
@@ -5,14 +5,9 @@ from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'houseofdota.settings')
-app = Celery('app')
+app = Celery('houseofdota')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
