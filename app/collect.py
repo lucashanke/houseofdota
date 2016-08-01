c4d097d964032492a9de24a43061ee593b190c4a
Lucas Hanke <hankelucas@gmail.com>
2016-08-01 18:43:18 -0300

    remove useless collect.py file

--- app/collect.py
+++
@@ -1,8 +0,0 @@
-from services.collector_service import CollectorService
-
-def main():
-    very_high_collector = CollectorService(3, ap=True, rap=True)
-    matches_recorded =very_high_collector.collect_from_last_100()
-
-if __name__ == "__main__":
-    main()
