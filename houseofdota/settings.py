c05d883fc8efb3ec58c822af0fddeee0ccf6690e
Lucas Hanke <hankelucas@gmail.com>
2016-08-01 12:15:57 -0300

    add setting for static files path

--- houseofdota/settings.py
+++ houseofdota/settings.py
@@ -119,11 +119,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
