75ce97d8df7171f248597010c2abe655f4d25fc2
Lucas Hanke <hankelucas@gmail.com>
2016-08-05 18:16:45 -0300

    add enzyme, mocha and chai for js testing

---
+++ app/assets/javascript/tests/components/AppBar_test.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactTestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
+import sinon from 'sinon';
+import { shallow, mount } from 'enzyme';
+import { expect } from 'chai';
+
+import AppBar from  '../../src/components/AppBar.jsx';
+import Popover from 'material-ui/Popover';
+import AppMenu from  '../../src/components/AppMenu.jsx';
+
+describe('<AppBar />', () => {
+  it('renders two children', () => {
+    const wrapper = shallow(<AppBar />);
+    expect(wrapper.find(AppMenu)).to.have.length(1);
+    expect(wrapper.find(Popover)).to.have.length(1);
+  });
+
+  it('renders closed popover', () => {
+    const wrapper = shallow(<AppBar />);
+    const popover = wrapper.find(Popover);
+    expect(popover.props().open).to.be.false;
+  });
+
+  it('opens popover once menu button is clicked', () => {
+    const wrapper = mount(<AppBar />);
+    const node = ReactDOM.findDOMNode(
+        ReactTestUtils.findRenderedDOMComponentWithTag(
+          wrapper.instance(), 'button'
+        )
+      );
+    ReactTestUtils.Simulate.touchTap(node);
+    const popover = wrapper.find(Popover);
+    expect(popover.props().open).to.be.true;
+  });
+});
---
+++ app/assets/javascript/tests/setup.js
@@ -0,0 +1,32 @@
+import jsdom from 'jsdom';
+import jQueryFactory from 'jquery';
+
+// Setup the simplest document possible.
+const doc = jsdom.jsdom();
+const win = doc.defaultView;
+
+// Set globals for mocha that make access to document and window feel
+// natural in the test environment.
+global.document = doc;
+global.window = win;
+global.$ = global.jQuery = jQueryFactory(global.window);
+global.navigator = {
+  userAgent: 'node.js'
+};
+
+// from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
+function propagateToGlobal (window) {
+  for (let key in window) {
+    if (!window.hasOwnProperty(key)) continue;
+    if (key in global) continue;
+
+    global[key] = window[key];
+  }
+}
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+// Take all properties of the window object and also attach it to the
+// mocha global object.
+propagateToGlobal(win);
