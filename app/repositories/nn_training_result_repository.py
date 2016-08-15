8162b4faa8babcea1c1259db92dc58a3c34d88ab
Lucas Hanke <hankelucas@gmail.com>
2016-08-15 16:53:06 -0300

    change nn performance endpoint to order results in desc end time

--- app/repositories/nn_training_result_repository.py
+++ app/repositories/nn_training_result_repository.py
@@ -4,4 +4,4 @@ class NnTrainingResultRepository:
 
     @staticmethod
     def fetch_from_patch(patch):
-        return NnTrainingResult.objects.filter(patch=patch).order_by('end_time')
+        return NnTrainingResult.objects.filter(patch=patch).order_by('-end_time')
