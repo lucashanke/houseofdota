5e2a98623b06ea96aaa69e059a2d40dc2b566c1d
Lucas Hanke <hankelucas@gmail.com>
2016-08-26 16:32:38 -0300

    add kill step before starting gunicorn, beat and worker

--- infra/scripts/start_celery_worker.sh
+++ infra/scripts/start_celery_worker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source venv/bin/activate
+ps -aux | grep 'celery -A houseofdota worker' | awk '{print $2}' | xargs kill -9
 celery -A houseofdota worker -l info
--- infra/scripts/start_gunicorn.sh
+++ infra/scripts/start_gunicorn.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source venv/bin/activate
+ps -aux | grep 'gunicorn houseofdota.wsgi -b 0.0.0.0:80 --log-file -' | awk '{print $2}' | xargs kill -9
 gunicorn houseofdota.wsgi -b 0.0.0.0:80 --log-file -
