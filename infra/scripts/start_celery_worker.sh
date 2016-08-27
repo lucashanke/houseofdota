c82692abfc9369abfb9e291d9b979412fdaf6652
Lucas Hanke <hankelucas@gmail.com>
2016-08-27 14:35:30 -0300

    add autoscale to celery worker

--- infra/scripts/start_celery_worker.sh
+++ infra/scripts/start_celery_worker.sh
@@ -2,4 +2,4 @@
 
 source venv/bin/activate
 ps -aux | grep 'celery -A houseofdota worker' | awk '{print $2}' | xargs kill -9
-celery -A houseofdota worker -l info
+celery -A houseofdota worker -l info --autoscale=10,3
