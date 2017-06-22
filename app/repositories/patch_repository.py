ee89835b87d470d751fd981f33e3264695cd0c48
Lucas Hanke <hankelucas@gmail.com>
2017-06-22 20:51:14 -0300

    add method to trieve patch from version

--- app/repositories/patch_repository.py
+++ app/repositories/patch_repository.py
@@ -1,4 +1,5 @@
 from app.models import Patch
+from django.core.exceptions import ObjectDoesNotExist
 
 class PatchRepository:
 
@@ -6,3 +7,10 @@ class PatchRepository:
     def fetch_current_patch(cls):
         patches = Patch.objects.all().order_by('-start_date')
         return patches[0] if len(patches) > 0 else None
+
+    @classmethod
+    def fetch_by_version(cls, version):
+        try:
+            return Patch.objects.get(pk=version)
+        except ObjectDoesNotExist as e:
+            return None
