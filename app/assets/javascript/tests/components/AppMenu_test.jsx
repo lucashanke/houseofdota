b31ee0870a84ab72e1521959ff89da4cd36bbce2
Lucas Hanke <hankelucas@gmail.com>
2017-07-01 15:56:29 -0300

    fix AppMenu tests

--- app/assets/javascript/tests/components/AppMenu_test.jsx
+++ app/assets/javascript/tests/components/AppMenu_test.jsx
@@ -7,15 +7,17 @@ import MenuItem from 'material-ui/MenuItem';
 
 describe('<AppMenu />', () => {
 
-  it('renders two items', () => {
+  it('renders three items', () => {
     const wrapper = shallow(<AppMenu />);
-    expect(wrapper.find(MenuItem)).to.have.length(4);
+    expect(wrapper.find(MenuItem)).to.have.length(3);
   });
 
   it('buttons have correct links', () => {
     const wrapper = shallow(<AppMenu />);
     const buttons = wrapper.find(MenuItem);
     expect(buttons.at(0).props().href).to.equal('/');
+    expect(buttons.at(1).props().href).to.equal('/statistics');
+    expect(buttons.at(2).props().href).to.equal('/recommendation');
   });
 
 });
