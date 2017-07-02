f5b97644a56afb2fe054289365583711d6156d79
Lucas Hanke <hankelucas@gmail.com>
2017-07-01 23:27:18 -0300

    add whitenoise to serve static file on prod

--- houseofdota/wsgi.py
+++ houseofdota/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise.django import DjangoWhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "houseofdota.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
