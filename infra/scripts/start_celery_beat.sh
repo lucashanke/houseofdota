737b4242ff4e852615e7c9e781d88a042d3b4a06
Lucas Hanke <hankelucas@gmail.com>
2017-07-01 22:54:31 -0300

    remove entire old infra folder <3

--- infra/scripts/start_celery_beat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-deactivate
-source /opt/houseofdota/venv/bin/activate
-source /opt/houseofdota/.env
-ps -aux | grep 'celery -A houseofdota beat' | awk '{print $2}' | xargs kill -9
-exec /opt/houseofdota/venv/bin/celery -A houseofdota beat -l info --logfile /var/log/celery/beat.log
