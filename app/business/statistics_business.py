e461f849c5073970e17e3ab7ce2d6c46e9dc2442
Lucas Hanke <hankelucas@gmail.com>
2017-07-10 18:36:12 -0300

    refactor bundle association build and update

--- app/business/statistics_business.py
+++ app/business/statistics_business.py
@@ -41,14 +41,16 @@ class StatisticsBusiness:
             self._previous_iteration = self._patch_statistics.iteration
         else:
             self._previous_iteration = 0
+        self._iteration = self._previous_iteration + 1
 
     def update_statistics(self):
-        matches = MatchRepository.fetch_from_patch(self._patch, max_matches=StatisticsBusiness.MAX_MATCHES)
+        matches = MatchRepository.fetch_from_patch(
+            self._patch, max_matches=StatisticsBusiness.MAX_MATCHES)
         self.update_patch_statistics(matches)
 
     def update_patch_statistics(self, matches):
         self._patch_statistics.match_quantity = len(matches)
-        self._patch_statistics.iteration = self._previous_iteration + 1
+        self._patch_statistics.iteration = self._iteration
         self._patch_statistics.save()
         self.update_pick_associations(matches)
         self.update_bundles_associations(matches)
@@ -58,39 +60,42 @@ class StatisticsBusiness:
         return self._patch_statistics
 
     def update_bundles_associations(self, matches):
-        heroes_rates = self._extract_statistics_from_association_rules(matches)
-        for hero_ids in heroes_rates['pick_rate'].keys():
-            self._update_bundle_association(hero_ids, heroes_rates)
+        bundle_associations = self.extract_bundle_association_rules(matches)
+        for bundle_association in bundle_associations:
+            self._save_bundle_rule(bundle_association)
 
-    def _extract_statistics_from_association_rules(self, matches):
-        heroes_rates = { 'pick_rate' : {}, 'confidence' : {} }
+    def extract_bundle_association_rules(self, matches):
+        bundle_associations = []
+        winning_associations = extract_apriori_association_rules(
+            construct_matches_list_for_winning_teams(matches), StatisticsBusiness.MAX_BUNDLE_SIZE)
+        for winning_association in winning_associations:
+            bundle_associations.append(self._build_bundle_association_rule(winning_association))
+        return bundle_associations
 
-        winning_data = extract_apriori_association_rules(self._construct_matches_list_for_winning_teams(matches), StatisticsBusiness.MAX_BUNDLE_SIZE)
-        for winning_association in winning_data:
-            hero_ids = get_apriori_association_heroes(winning_association)
-            heroes_rates['confidence'][hero_ids] = winning_association.support
-            heroes_rates['pick_rate'][hero_ids] = 1
+    def _build_bundle_association_rule(self, association):
+        hero_ids = get_apriori_association_heroes(association)
+        bundle_rule = BundleAssociationRules(
+            hero_bundle=hero_ids,
+            bundle_size = len(hero_ids.split(',')),
+            pick_rate = 0,
+            win_rate = 0,
+            confidence = association.support,
+            iteration = self._iteration,
+        )
+        return bundle_rule
 
-        return heroes_rates
-
-    def _update_bundle_association(self, hero_ids, rates):
-        bundle_association = BundleAssociationRules(hero_bundle=hero_ids,
-            patch_statistics=self._patch_statistics)
-        bundle_association.pick_rate = rates['pick_rate'][hero_ids]
-        bundle_association.win_rate = rates['confidence'][hero_ids]/rates['pick_rate'][hero_ids]
-        bundle_association.confidence = rates['confidence'][hero_ids]
-        bundle_association.bundle_size = len(hero_ids.split(','))
-        bundle_association.iteration = self._previous_iteration + 1
+    def _save_bundle_rule(self, bundle_association):
+        bundle_association.patch_statistics = self._patch_statistics
         bundle_association.save()
 
     def update_counters_associations(self, matches):
-        counter_associations = self._extract_counter_association_rules(matches)
+        counter_associations = self.extract_counter_association_rules(matches)
         for counter_association in counter_associations:
             self._save_counter_rule(counter_association)
 
-    def _extract_counter_association_rules(self, matches):
+    def extract_counter_association_rules(self, matches):
         apriori_associations = extract_apriori_association_rules(
-            self._construct_matches_list(matches, counter_pick=True), 2
+            construct_matches_list(matches, counter_pick=True), 2
         )
         counter_associations = []
         for association in apriori_associations:
@@ -108,7 +113,7 @@ class StatisticsBusiness:
             confidence_counter = association.ordered_statistics[0].confidence,
             confidence_hero = association.ordered_statistics[1].confidence,
             lift = association.ordered_statistics[0].lift,
-            iteration = self._previous_iteration + 1
+            iteration = self._iteration
         )
         return counter_rule
 
@@ -117,13 +122,13 @@ class StatisticsBusiness:
         counter_rule.save()
 
     def update_pick_associations(self, matches):
-        pick_associations = self._extract_pick_association_rules(matches)
+        pick_associations = self.extract_pick_association_rules(matches)
         for pick_association in pick_associations:
             self._save_pick_rule(pick_association)
 
-    def _extract_pick_association_rules(self, matches):
+    def extract_pick_association_rules(self, matches):
         apriori_associations = extract_apriori_association_rules(
-            self._construct_teams_list(matches),
+            construct_teams_list(matches),
             StatisticsBusiness.MAX_BUNDLE_SIZE,
             min_support=0.00005
         )
@@ -138,7 +143,7 @@ class StatisticsBusiness:
             hero_bundle=hero_ids,
             bundle_size=len(hero_ids.split(',')),
             support = association.support*2,
-            iteration = self._previous_iteration + 1
+            iteration = self._iteration
         )
         return pick_rule
 
@@ -154,7 +159,7 @@ class StatisticsBusiness:
     def update_winning_statistics(self):
         for bundle_association in self._patch_statistics.bundle_rules.all():
             winning_bundle_statistics = calculate_from_association_rule(bundle_association)
-            winning_bundle_statistics.iteration = self._previous_iteration + 1
+            winning_bundle_statistics.iteration = self._iteration
             winning_bundle_statistics.save()
         self._patch_statistics.winning_bundles_statistics.filter(
             iteration=self._previous_iteration
