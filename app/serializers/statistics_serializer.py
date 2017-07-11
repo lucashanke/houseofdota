c2614ec1335eb3640cad9b058614045fe3e6547a
Lucas Hanke <hankelucas@gmail.com>
2017-07-11 13:30:45 -0300

    add serializer for WinningBundleStatistics

--- app/serializers/statistics_serializer.py
+++ app/serializers/statistics_serializer.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
+from app.models import WinningBundleStatistics
+from app.business.statistics_business import get_heroes_from_association
+
+class WinningBundleStatisticsSerializer(serializers.ModelSerializer):
+    heroes = serializers.JSONField()
+    class Meta:
+        model = WinningBundleStatistics
+        fields = '__all__'
 
 class BundleAssociationRulesSerializer(serializers.Serializer):
     match_quantity = serializers.IntegerField()
-    statistics = serializers.JSONField()
+    patch = serializers.CharField()
+    statistics = serializers.ListField(child=WinningBundleStatisticsSerializer())
 
 class CounterAssociationRulesSerializer(serializers.Serializer):
     match_quantity = serializers.IntegerField()
@@ -10,3 +19,8 @@ class CounterAssociationRulesSerializer(serializers.Serializer):
 
 class ListCounterPickSerializer(serializers.Serializer):
     results = serializers.ListField(child=CounterAssociationRulesSerializer())
+
+class BundleRecommendationSerializer(serializers.Serializer):
+    match_quantity = serializers.IntegerField()
+    patch = serializers.CharField()
+    statistics = serializers.JSONField()
