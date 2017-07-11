48e633b2021cff260238c4ca75dad3ad2e5be9cc
Lucas Hanke <hankelucas@gmail.com>
2017-07-11 14:32:30 -0300

    change recommendation view to use recommendation service and serializers

--- app/api/recommendation_view.py
+++ app/api/recommendation_view.py
@@ -1,14 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from app.services.statistics_service import StatisticsService
-from app.serializers.statistics_serializer import *
+from app.services.recommendation_service import RecommendationService
+from app.serializers.recommendation_serializer import *
 from app.repositories.patch_repository import PatchRepository
 
 @api_view()
 def hero_recommendations_for_bundle(request):
     hero_ids = request.query_params.get('hero_ids[]')
-    heroes_statistics = StatisticsService(
+    heroes_statistics = RecommendationService(
         patch=PatchRepository.fetch_current_patch()
     ).get_bundle_recommendations(
         hero_ids=[] if hero_ids is None else [str(hero) for hero in hero_ids.split(',')]  ,
@@ -20,11 +20,11 @@ def hero_recommendations_for_bundle(request):
 def counter_recommendations_for_hero(request):
     hero_ids = request.query_params.get('hero_ids[]')
     counter_pick_statistics = {
-        'results': StatisticsService(
+        'results': RecommendationService(
             patch=PatchRepository.fetch_current_patch()
-        ).get_counter_pick_statistics(
+        ).get_counter_recommendations(
             hero_ids=[] if hero_ids is None else [str(hero) for hero in hero_ids.split(',')]  ,
         ),
     }
-    serializer = ListCounterPickSerializer(counter_pick_statistics)
+    serializer = ListCounterRecommendationSerializer(counter_pick_statistics)
     return Response(serializer.data)
