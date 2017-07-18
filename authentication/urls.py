8a90a12582ea5faa039216e8ec69c9425553abf6
Lucas Hanke <hankelucas@gmail.com>
2017-07-18 18:20:02 -0300

    fix import on authentication urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
-from authentication.views import IndexView, LogoutView
+from authentication.views import LogoutView
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index'),
     url(r'^logout', login_required(LogoutView.as_view(), login_url='/'), name='logout')
 ]
