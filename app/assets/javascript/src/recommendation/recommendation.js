088868e6a6042db5f63b1f75024940a4c99f7fd5
Lucas Hanke <hankelucas@gmail.com>
2017-07-26 11:24:48 -0300

    rename variables on recommendation.js

--- app/assets/javascript/src/recommendation/recommendation.js
+++ app/assets/javascript/src/recommendation/recommendation.js
@@ -3,37 +3,37 @@ export const getCounters = (results, unavailableHeroes) => {
     return allCounters.concat(heroCounter.counterPicks);
   }, [])
   counters = _.orderBy( counters ,['counterCoefficient'], ['desc']);
-  let selectedCounters = [];
+  let recommendedCounters = [];
   for (let i = 0; i < counters.length; i++){
     const counterId = counters[i].id;
     if(!unavailableHeroes.includes(counterId)) {
-      if(!selectedCounters.map(s => s.id).includes(counterId)) {
-        selectedCounters.push({
+      if(!recommendedCounters.map(s => s.id).includes(counterId)) {
+        recommendedCounters.push({
           id: counters[i].id,
           name: counters[i].name,
           counterFor: 1,
         });
       } else {
-        selectedCounters.filter(c => c.id === counterId)[0].counterFor++;
+        recommendedCounters.filter(c => c.id === counterId)[0].counterFor++;
       }
     }
   }
-  return selectedCounters.slice(0,5);
+  return recommendedCounters.slice(0,5);
 }
 
 export const getAllies = (bundles, unavailableHeroes) => {
   const orderedBundles = _.orderBy(bundles, ['bundleSize','confidence'], ['desc', 'desc']);
-  let selectedRecommended = [];
-  for (let i = 0; i < orderedBundles.length && selectedRecommended.length < 5; i++){
+  let recommendedAllies = [];
+  for (let i = 0; i < orderedBundles.length && recommendedAllies.length < 5; i++){
     const recommendedId = orderedBundles[i].recommended[0].id;
     if(!unavailableHeroes.includes(recommendedId)
-      && !selectedRecommended.map(s => s.id).includes(recommendedId)) {
-        selectedRecommended.push({
+      && !recommendedAllies.map(s => s.id).includes(recommendedId)) {
+        recommendedAllies.push({
           id: orderedBundles[i].recommended[0].id,
           name: orderedBundles[i].recommended[0].name,
           bundleSize: orderedBundles[i].bundleSize,
         });
     }
   }
-  return selectedRecommended;
+  return recommendedAllies;
 }
